2d79b3371038c80d229efc2daee84e8a4ba70d99
Chandan Kumar Mallick <chandansoumya28@gmail.com>
2023-09-30 21:34:26 +0530

    Updated ReadME

--- src/router/index.js
+++ src/router/index.js
@@ -6,69 +6,69 @@ import { Auth, Hub } from "aws-amplify";
 
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import("@/views/Home.vue"),
-    // Add meta line below
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/signin",
-    name: "Signin",
-    component: () => import("@/views/Signin.vue"),
-    //path: '/',
-    //component: () => import('@/layouts/default/Default.vue'),
-    //children: [
-    //{
-    // path: '',
-    // name: 'Home',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-    //},
-    //],
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
+        // Add meta line below
+        meta: { requiresAuth: true },
+    },
+    {
+        path: "/signin",
+        name: "Signin",
+        component: () => import("@/views/Signin.vue"),
+        //path: '/',
+        //component: () => import('@/layouts/default/Default.vue'),
+        //children: [
+        //{
+        // path: '',
+        // name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        //component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        //},
+        //],
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 // Amplify supplies a Hub that listens for events, we are interested in sign-in and sign-out events
 Hub.listen("auth", async (data) => {
-  if (data.payload.event === 'signOut') {
-    router.push({ path: '/signin' });
-  } else if (data.payload.event === 'signIn') {
-    router.push({ path: '/' });
-  }
+    if (data.payload.event === 'signOut') {
+        router.push({ path: '/signin' });
+    } else if (data.payload.event === 'signIn') {
+        router.push({ path: '/' });
+    }
 });
 
 // Adding method to chech if user is authenticated before moving navigating to the requested page
 router.beforeEach(async (to, from, next) => {
-  // Check if the route requires authentication by examining requiresAuth
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    try {
-      // Call the Amplify Auth method to check if the user is authenticated.
-      const user = await Auth.currentAuthenticatedUser();
-      if (!user) {
-        // Not authenticated, so redirect to signin page.
-        next("/signin");
-      }
-      else {
-        // They are authenticated, so we can continue.
+    // Check if the route requires authentication by examining requiresAuth
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        try {
+            // Call the Amplify Auth method to check if the user is authenticated.
+            const user = await Auth.currentAuthenticatedUser();
+            if (!user) {
+                // Not authenticated, so redirect to signin page.
+                next("/signin");
+            }
+            else {
+                // They are authenticated, so we can continue.
+                next();
+            }
+        } catch (err) {
+            // Error, so redirect to signin page.
+            next("/signin");
+        }
+    } else {
+        // Route does not require authentication, so we can continue.
         next();
-      }
-    } catch (err) {
-      // Error, so redirect to signin page.
-      next("/signin");
     }
-  } else {
-    // Route does not require authentication, so we can continue.
-    next();
-  }
 });
 // End of new method
 export default router;
